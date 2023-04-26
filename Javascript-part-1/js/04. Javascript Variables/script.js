53d724488d9685b1c9a698e451cf0daebf3c5255
mohammedyaseen2211 <ysn6040@gmail.com>
2023-04-26 23:48:22 +0530

    javascript variables completed

---
+++ Javascript-part-1/js/04. Javascript Variables/script.js
@@ -0,0 +1,32 @@
+// var keyword is known as variables. variables also called as containers/identifers
+
+var digit =5;
+
+var x = 2;
+var z = digit + x ;
+
+alert(digit);
+
+console.log(z);
+
+
+// es6 variables : let, const
+
+let a =3;
+
+//for let we can reassign the variable but can't redeclare.
+
+a =5; 
+
+console.log(a)
+
+
+// for const we can't redecalre and reassign
+
+const b = 10 ;
+// wrong  :- const b =12 ; or b=12;
+
+console.log(b)
+
+// Note:-  reserved  words cant use such as let let, boolean ... etc.
+
