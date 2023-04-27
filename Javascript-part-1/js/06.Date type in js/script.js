6733e8499a6386a5993abe2c623cb9bbdd5dd1cb
mohammedyaseen2211 <ysn6040@gmail.com>
2023-04-27 14:58:13 +0530

    datatype completed

---
+++ Javascript-part-1/js/06.Date type in js/script.js
@@ -0,0 +1,68 @@
+let x = 5;
+// here 5 is a data type which is in number
+let y = "5";
+// here 5 is a data type which is in string.
+
+//  this is dynamic data variable we can store num and we can assign string.
+
+let a = "hello";
+// a = 3 ;
+console.log(a);
+
+// double quotation and single quotation 
+
+let q = "hello";
+
+let q1 = 'hello';
+console.log(q + q1)
+// here double and single quotation working because if we want add quotation between the text.
+
+q = "hel'lo" // if we want single quotation b/w text we have to use double quotation
+
+console.log(q);
+
+q1 = 'hel"lo' //if we want double quotation b/w text we have to use single quotation;
+console.log(q1);
+
+// otherway of printing  double quotation b/w the text using double quotation.
+
+q1 = "hey \"dude !";
+console.log(q1);
+
+// -----------------numbers--------------------------
+
+let num2 =34.00;
+console.log(num2);
+
+//----- boolean :true or false -- for comparing we use boolean
+
+let bool1 = 2>3;
+console.log(bool1)
+
+
+// array is data type and also called non-primitve data types
+
+let arr = ["a", "b", "c", "d"];
+
+console.log(arr);
+
+// object
+
+let obj = {
+name:"csk",
+location:"chennai"
+}
+
+// undefined:if value is declared but not assigned is  called undefined
+
+let und ;
+console.log(und);
+
+// typeOf operator is used to check the type
+
+let t1 = typeof 1;
+let t2 = typeof "1";
+let t3 = typeof [1,2];
+let t4 = typeof {a:3, b:5};
+
+
