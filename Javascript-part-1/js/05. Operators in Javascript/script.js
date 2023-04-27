7dba979b2b9915d05e87f22fdb7367dd063b76a1
mohammedyaseen2211 <ysn6040@gmail.com>
2023-04-27 14:28:55 +0530

    operator completed

---
+++ Javascript-part-1/js/05. Operators in Javascript/script.js
@@ -0,0 +1,88 @@
+let x;
+
+x = 10;
+
+// x is assign to y
+let y = 5;
+x = y;
+console.log("x value is " + x)
+
+// addition operator
+
+let z = x + y;
+console.log(z);
+
+// subtractor operator
+
+
+let z1 = x - y;
+console.log(z1);
+
+// multiplication operator
+
+let z4 = x * y;
+console.log(z4);
+
+// divison  operator
+
+
+let z3 = x / y;
+console.log(z3);
+
+// modulus operator using for remainder of that items
+
+
+let z2 = x % y;
+console.log(z2);
+
+//  the things are same we write in different ways
+
+let y1;
+y1 = 6;
+y1 = y1 + 2;
+//------------------ or---------------------
+// we can write like this too
+y1 += 2;
+console.log(y1);
+
+// powers
+
+let num = 4;
+
+num = num ** 3;
+
+console.log(num);
+
+
+// Data types comparison
+
+
+let num1 = "4"; //this is string
+
+console.log(num + num1) // numbers not added but here the concat each other
+
+// increment operator
+num++; // this one is increase by 1. i.e num + 1 ;
+console.log(num)
+
+//here the string value is number we can convert string to number by one.
+
+num1++;
+console.log(num1);
+
+
+// Decrement operator
+
+num1--; //decrease by 1;
+console.log(num1)
+
+// bodmas rule
+
+let b = 5;
+
+let b1 = b + 5 / 5 ;
+console.log(b1); // oupput :- 6 ;
+
+let b2 = (b + 5 )/ 5 ;
+console.log(b2); // oupput :- 2 ;
+
