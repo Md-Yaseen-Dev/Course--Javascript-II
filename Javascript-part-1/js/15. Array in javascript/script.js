3e58e4325da56293269315ca9d4763d522680565
mohammedyaseen2211 <ysn6040@gmail.com>
2023-05-01 13:09:07 +0530

    arrays completed

---
+++ Javascript-part-1/js/15. Array in javascript/script.js
@@ -0,0 +1,16 @@
+/*-----------------Arrays----------------------*/
+
+// arrays are used to store multiple items in contiguous(next to each other)memory location. Array index start from 0 ll
+
+const book  = ["Math", "Physics", "Bio","computer-science"];
+
+// ---------------other method to declare array--------
+
+const book1 = new Array("Math", "Physics", "Bio","computer-science");
+console.log(book1[2]); // for bio 
+
+
+// for upadate the value or replace the value with other
+
+book[2]= "Hindi";
+console.log(book)
