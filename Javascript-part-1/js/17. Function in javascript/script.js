1d93a2ee84283845cf0c0bbc7d87b63c3921e334
mohammedyaseen2211 <ysn6040@gmail.com>
2023-05-02 21:26:10 +0530

    function in js completed

---
+++ Javascript-part-1/js/17. Function in javascript/script.js
@@ -0,0 +1,15 @@
+// multiplication of number
+
+function MulTable(a){
+
+    for (i = 1; i <= 10; i++) {
+
+        document.write(`${a} X ${i} = ${a * i}<br>`);
+    }
+    
+}
+MulTable(2);
+document.write('<br>')
+MulTable(3);
+
+
