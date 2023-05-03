14c8ffabc2196be4c74ef07e98c4c51c6f9b301c
mohammedyaseen2211 <ysn6040@gmail.com>
2023-05-03 21:49:27 +0530

    objects in js completed

---
+++ Javascript-part-1/js/24. Objects in javascript/script.js
@@ -0,0 +1,14 @@
+/**---------------------object------------------ */
+
+// object has properties, action or function
+
+
+// ------------------objects in programming----------------
+
+
+// student, teacher, course in learning management system
+
+// Account, Account holder, cashier, bank manager etc in banking system
+
+// Book, department etc in library managment system.
+
