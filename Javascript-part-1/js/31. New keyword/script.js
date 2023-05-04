4ec397b0b7fe138227238d225f271ba70af22e79
mohammedyaseen2211 <ysn6040@gmail.com>
2023-05-04 21:29:07 +0530

    new keyword completed

---
+++ Javascript-part-1/js/31. New keyword/script.js
@@ -0,0 +1,18 @@
+var person = {
+    name: "shaik",
+    age: 21,
+
+}
+
+// for accessing the object
+
+console.log(person.name);
+
+
+// another way of creating a object using new keyword
+
+var Person1 = new Object();
+
+Person1.name = "roshan zameer";
+
+console.log(Person1.name);
