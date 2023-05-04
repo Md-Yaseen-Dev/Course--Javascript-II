c3442f8342e431a0d7d56f4a1bbbea4b34281f54
mohammedyaseen2211 <ysn6040@gmail.com>
2023-05-04 16:36:25 +0530

    math object completed

---
+++ Javascript-part-1/js/28.Math Object/script.js
@@ -0,0 +1,66 @@
+// alreadb some objects are created that is known as Math object
+
+/**--------------Math properties----------------- */
+
+
+// ---------------PI- Number----------------------
+
+console.log(Math.PI); //3.14159265.......
+
+
+// ---------------Eulers number---------------------
+
+console.log(Math.E); //2.7182812.....
+
+
+// ----------------Math.Round-----------------------
+
+var a = Math.round(4.5);
+
+console.log(a); // if it is more than  4.4 it will get 5 , if it less than 4.5 output will be 4.
+
+
+// ---------------Math.ceil-------------------------
+
+
+var b = Math.ceil(4.01);
+console.log(b); // if the point is more than zero. output will be 5.it means it gives upper limit
+
+
+// ---------------Math.floor-------------------------
+
+var c = Math.floor(4.9);
+
+console.log(c); // if it near to 5 . but it will give lowe limit i.e 4
+
+
+// ------------------Math.trumc--------------------
+
+var d = Math.trunc(4.7);
+console.log(d); // it will exclude point. that means it will not count after that point whatever it may be.
+
+
+// -------------------Math.Pow------------------
+
+var e = Math.pow(2, 3);
+
+console.log(e);// it will be just like 2³ = 2*l2*2
+
+
+// ------------------Math.sqrt---------------------
+
+var f = Math.sqrt(100);
+
+console.log(d); // √100 = 10
+
+
+// -----------------Math.min----------------------------
+
+
+var g = Math.min(2, 3, 5, 6, 77, 3, 52, -12, 5, -1);
+
+console.log(g); // gives minimum value
+
+// -----------------Math.max----------------------------
+var h = Math.min(2, 3, 5, 6, 77, 3, 52, -12, 5, -1);
+console.log(h); // gives maximum value
