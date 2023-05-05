88a9b1dbe85383ce39568801c4a900b92a2b88e3
mohammedyaseen2211 <ysn6040@gmail.com>
2023-05-05 22:43:23 +0530

    DOM completed

---
+++ Javascript-part-1/js/37. DOM - Document Object Model/script.js
@@ -0,0 +1,30 @@
+// The Document Object model(DOM) is an application programming interface API for manipulating HTML and XML documents.(add,remove, and modify parts of the document/HTML)
+
+
+// when a web page is loaded, the browser creates a document object model of the page.
+
+// 1.------------<!DOCTYPE html>----------------
+// 2.------------<html lang="en">-------------------
+// 3.--------------<head>// </head>---------------------
+//3. -------------- <body> </body>-------------------------
+// 4.------------------title-------------------------
+// 4.------------------style-------------------------
+
+
+
+/** with the object  model. Javascirpt gets all the power it needs to create dynamic HTML*/
+
+// javascript can change all the HTML elements in the page
+// javascript can change all the HTML attribute in the page
+// javascript can change all the css styles in the page
+// javascript can remoce existing HTML elements and attribute in the page
+// javascript can change add new existing HTML elements and attribute in the page
+// javascript can react to all existing HTML events in the page.
+// javascript can create new HTML events in the page
+
+
+/** Note in the DOM, all HTML elements are defined as objects. so it will have both property and*/
+
+/** The document object represents your web page */
+
+/**If you want to access any element in an HTML page, you always start with accessing the document object */
