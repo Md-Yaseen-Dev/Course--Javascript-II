f7806ed1f5a9253458905994f976e032998ed06e
mohammedyaseen2211 <ysn6040@gmail.com>
2023-05-05 17:05:47 +0530

    nested object completed

---
+++ Javascript-part-1/js/35. nested object/script.js
@@ -0,0 +1,17 @@
+var user = {
+
+    id: 101,
+    email: "abc@gmail.com",
+    personalInfo: {
+        name: "abc",
+        address: {
+            street: "dsgsgs",
+            city: "delhi",
+            country: "India"
+        }
+    }
+}
+
+
+console.log(user);
+console.log(user.personalInfo.address);
